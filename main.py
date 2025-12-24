cd2e796030b1987e9f1255af085d4bbddd5bd1ca
DaMaGi <35659412+DaMaGi@users.noreply.github.com>
2025-12-23 21:29:10 -0300

    add book files, change book_path to allow different entries

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-from stats import word_count, char_count, dict_sort, sort_on
+from stats import word_count, char_count, dict_sort
+import sys
 
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
     
 def main():
-    book_path = "./books/frankenstein.txt"
+    print(f"============ BOOKBOT ============")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     word_num = word_count(text)
-    print(f"============ BOOKBOT ============\nAnalyzing book found at {book_path}...\n----------- Word Count ----------\nFound {word_num} total words\n--------- Character Count -------")
+    print(f"Analyzing book found at {book_path}...\n----------- Word Count ----------\nFound {word_num} total words\n--------- Character Count -------")
     for letter in dict_sort(char_count(text)):
         print(f"{letter['char']}: {letter['num']}")
     print("============= END ===============")
